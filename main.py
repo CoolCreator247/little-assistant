23d0ae2539a849a2a464e3e7c1bc747dbf23d92c
Faris Allafi <farisqaisallafi@gmail.com>
2022-02-13 16:14:54 +0000

    first edit

---
+++ main.py
@@ -0,0 +1,20 @@
+
+print("Hi, my name is little assistant ")   
+print("I am here to help")
+print("you can tell or ask me anything")
+print("╭(◔ ◡ ◔)/ ")
+
+try:
+    input = reply 
+    if reply == "I am happy":
+        print("I am too")
+    elif reply == "I am sad":
+        print("Sorry are you ok")
+        print("Why are you sad?")
+        input(">")
+    elif reply == "Are you happy?":
+        print("I am happy thanks for asking")
+
+
+except:
+    print("Nothing")
